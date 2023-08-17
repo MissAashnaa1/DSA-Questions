2cf6d2806e54024dc8f71121c7e2ce48b604c5b7
MissAashnaa1 <97970004+MissAashnaa1@users.noreply.github.com>
2023-08-17 15:17:39 +0530

    Updated Solution - GfG to GitHub

--- Medium/Floyd Warshall/floyd-warshall.cpp
+++ Medium/Floyd Warshall/floyd-warshall.cpp
@@ -15,31 +15,28 @@ class Solution {
         
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j] == -1){
+                if(matrix[i][j]==-1)
                     matrix[i][j]=1e9;
-                }
-                if(i==j){
+                else if(i==j)
                     matrix[i][j]=0;
-                }
             }
         }
-        
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
                 for(int j=0;j<n;j++){
-                    matrix[i][j]=min(matrix[i][j], (matrix[i][k]+matrix[k][j]));
+                    matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j]);
                 }
             }
         }
         
-       for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j] == 1e9){
-                    matrix[i][j]=-1;
+        
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                   if(matrix[i][j]==1e9)
+                        matrix[i][j]=-1;
                 }
             }
-        }
-    
+        
 	}
 };
 
