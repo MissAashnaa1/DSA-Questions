f037f99e06d14cd8382010097ccf8b8fb86689c2
MissAashnaa1 <97970004+MissAashnaa1@users.noreply.github.com>
2023-08-24 07:09:21 +0530

    Time: 0 ms (100%), Space: 6.5 MB (22.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -14,31 +14,48 @@ class Solution {
     //     }
     //     return way1+way2;
     // }
-    int helper(int m,int n,int i,int j,vector<vector<int>>&dp){
-        if(i>m||j>n)
-            return 0;
-        if(i==m&&j==n){
-            return 1;
-        }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int way1=0;
-        int way2=0;
-        if(i<m)
-            way1=helper(m,n,i+1,j,dp);
-        if(j<n)
-            way2=helper(m,n,i,j+1,dp);
-        return dp[i][j]=way1+way2;
-    }
+    // int helper(int m,int n,int i,int j,vector<vector<int>>&dp){
+    //     if(i>m||j>n)
+    //         return 0;
+    //     if(i==m&&j==n){
+    //         return 1;
+    //     }
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     int way1=0;
+    //     int way2=0;
+    //     if(i<m)
+    //         way1=helper(m,n,i+1,j,dp);
+    //     if(j<n)
+    //         way2=helper(m,n,i,j+1,dp);
+    //     return dp[i][j]=way1+way2;
+    // }
 public:
     int uniquePaths(int m, int n) {
         
         // int ans=rec(m-1,n-1,0,0);
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        dp[m-1][n-1]=0;
-        int ans=helper(m-1,n-1,0,0,dp);
+        // dp[m-1][n-1]=0;
+        // int ans=helper(m-1,n-1,0,0,dp);
+       
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0){
+                    dp[i][j]=1;
+                }
+                else{
+                    int up=0;
+                    int right=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        right=dp[i][j-1];
+                    dp[i][j]=up+right;
+                }
+            }
+        }
 
-        return ans;
+        return dp[m-1][n-1];
     
     }
 };
