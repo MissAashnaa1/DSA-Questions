3717f6908ea74c2fbb2f2785a4278b6b9ffc7629
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-05 04:55:18 +0530

    Time: 28 ms (49.93%), Space: 6.4 MB (71.01%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool safe(vector<vector<char>>& board,int r,int c,char ch){
+        for(int i=0;i<9;i++){
+            if(board[i][c]==ch)
+                return false;
+            if(board[r][i]==ch)
+                return false;
+            if(board[(r/3)*3+(i/3)][(c/3)*3+(i%3)]==ch)
+                return false;
+        }
+        return true;
+    }
+    bool helper(vector<vector<char>>& board){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]=='.'){
+                    for(char ch='1';ch<='9';ch++){
+                        if(safe(board,i,j,ch)){
+                            board[i][j]=ch;
+                               if(helper(board)) 
+                                    return true;
+                               else
+                            board[i][j]='.';
+                        }
+                        
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        helper(board);
+        // return board;
+    }
+};
