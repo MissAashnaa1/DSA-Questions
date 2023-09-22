d249e0626986d7d18b1084ffe2c830bf61f9ee69
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-23 00:13:24 +0530

    Time: 16 ms (37.09%), Space: 9.3 MB (26.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -22,8 +22,28 @@ class Solution {
     }
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.length(),vector<int>(word2.length(),-1));
-        return helper(word1,word2,word1.size()-1,word2.size()-1,dp);
+        vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,-1));
+        // return helper(word1,word2,word1.size()-1,word2.size()-1,dp);
+    for(int i=0;i<=word1.length();i++){
+        dp[i][0]=i;
 
+    }
+    for(int i=0;i<=word2.length();i++){
+        dp[0][i]=i;
+        
+    }
+    for(int i=1;i<=word1.length();i++){
+        for(int j=1;j<= word2.length();j++){
+            if(word1[i-1]==word2[j-1])
+                dp[i][j]=dp[i-1][j-1];
+            else{
+                int inser=1+dp[i][j-1];
+                int delet=1+dp[i-1][j];
+                int replace=1+dp[i-1][j-1];
+            dp[i][j]=min(replace,min(inser,delet));
+            }
+        }
+    }
+    return dp[word1.length()][word2.length()];
     }
 };
