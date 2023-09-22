79dc4458cecfcfec548b2a86abce4e9bb2e5d06f
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-22 07:32:03 +0530

    Time: 38 ms (80.59%), Space: 7 MB (88.01%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    int helper(string text1, string text2) {
+        vector<int>prev(text1.size()+1,0);
+        vector<int>curr(text1.size()+1,0);
+        for(int i=1;i<=text1.size();i++){
+            for(int j=1;j<=text2.size();j++){
+                if(text1[i-1]==text2[j-1])
+                    curr[j]=1+prev[j-1];
+                else 
+                    curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return curr[text2.size()];
+    }
+public:
+    int minInsertions(string s) {
+        string str=s;
+        reverse(s.begin(),s.end());
+        return s.length()-helper(s,str);
+    }
+};
