da9c58569625299c2a89a8946024abe7771b83f5
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-22 13:37:23 +0530

    Time: 48 ms (27.93%), Space: 30.6 MB (20.76%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,24 +1,35 @@
 class Solution {
-    int helper(string s,string t, vector<vector<int>>&dp,int ind1,int ind2 ){
-        if(ind2<0){
-            return 1;
-        }
-        if(ind1<0){
-            return 0;
-        }
+    // int helper(string s,string t, vector<vector<int>>&dp,int ind1,int ind2 ){
+    //     if(ind2<0){
+    //         return 1;
+    //     }
+    //     if(ind1<0){
+    //         return 0;
+    //     }
         
-        if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-        }
-        if(s[ind1]==t[ind2])
-        return dp[ind1][ind2]=helper(s,t,dp,ind1-1,ind2-1)+helper(s,t,dp,ind1-1,ind2);
-        else return dp[ind1][ind2]=helper(s,t,dp,ind1-1,ind2);
-    }
+    //     if(dp[ind1][ind2]!=-1){
+    //         return dp[ind1][ind2];
+    //     }
+    //     if(s[ind1]==t[ind2])
+    //     return dp[ind1][ind2]=helper(s,t,dp,ind1-1,ind2-1)+helper(s,t,dp,ind1-1,ind2);
+    //     else return dp[ind1][ind2]=helper(s,t,dp,ind1-1,ind2);
+    // }
 public:
     int numDistinct(string s, string t) {
-        vector<vector<int>>dp(s.size(),vector<int>(t.size(),-1));
+        vector<vector<unsigned long long>>dp(s.size()+1,vector<unsigned long long>(t.size()+1,0));
 
-        return helper(s,t,dp,s.length()-1,t.length()-1);
-
+        // return helper(s,t,dp,s.length()-1,t.length()-1);
+        for(int i=0;i<=s.size();i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<=s.size();i++){
+            for(int j=1;j<=t.size();j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];// milgya yh manke aur nhi mila yh manke
+                }
+                else dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[s.size()][t.size()];
     }
 };
