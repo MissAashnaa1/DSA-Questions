fc930af2b3bee2984a4dc907a9cf3b6c0392b4bb
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-24 03:44:31 +0530

    Time: 7 ms (27.02%), Space: 11.6 MB (71.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    vector<vector<int>>dp(prices.size()+2,vector<int>(2,0));
+     vector<int> cur(2,0);
+    vector<int> front1(2,0);
+    vector<int> front2(2,0);
     //   return  helper(prices,0,1,dp);
     for(int index=prices.size()-1;index>=0;index--){
         for(int buy=0;buy<=1;buy++){
                   if(buy==1){//can buy
-            int buyy=-prices[index]+dp[index+1][0];//bought
-            int keep=dp[index+1][1];//keep dont buy new
-             dp[index][buy]=max(buyy,keep);//return jo max de
+            int buyy=-prices[index]+front1[0];//bought
+            int keep=front1[1];//keep dont buy new
+             cur[buy]=max(buyy,keep);//return jo max de
         }
         else{
-        int sell=prices[index]+dp[index+2][1];//sold
+        int sell=prices[index]+front2[1];//sold
 
-        int keep=dp[index+1][0];//keep
-             dp[index][buy]=max(sell,keep);
+        int keep=front1[0];//keep
+             cur[buy]=max(sell,keep);
         }
         //buy nhi krskte ab sell krskte hain ya purana hi rkh skte
         }
+        front2=front1;
+        front1=cur;
     }
-    return dp[0][1];
+    return cur[1];
     }
 };
