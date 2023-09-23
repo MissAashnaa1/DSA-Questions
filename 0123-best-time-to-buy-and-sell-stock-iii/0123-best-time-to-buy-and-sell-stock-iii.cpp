704301aa38642ef86259877a45c1b835a8b53b10
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-24 02:49:54 +0530

    Time: 651 ms (7.38%), Space: 219.1 MB (8.88%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,28 +2,30 @@ class Solution {
 
 public:
     int maxProfit(vector<int>& prices) {
-        vector<vector<vector<int>>>dp(prices.size()+1,             vector<vector<int>>(2,
-        vector<int>(4,0)));
+        // vector<vector<vector<int>>>dp(prices.size()+1,             vector<vector<int>>(2,
+        // vector<int>(4,0)));
+        vector<vector<int>>next(2,vector<int>(3,0));
         for(int index=prices.size()-1;index>=0;index--){
+            vector<vector<int>>curr(2,vector<int>(3,0));
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap>=0;cap--){
-    if(buy==1){//cap<2
-            int buyy=-prices[index]+dp[index+1][0][cap];
+        if(buy==1){//cap<2
+            int buyy=-prices[index]+next[0][cap];
             //3
-            int keep=dp[index+1][1][cap];// 1
-            dp[index][buy][cap]= max(buyy,keep);
+            int keep=next[1][cap];// 1
+            curr[buy][cap]= max(buyy,keep);
         }
         //buy==0
         else{
 
-            int sell=prices[index]+dp[index+1][1][cap+1];
-            int keep=dp[index+1][0][cap];// 2
-             dp[index][buy][cap]= max(sell,keep);                
-        }
-                }
+            int sell=prices[index]+next[1][cap+1];
+            int keep=next[0][cap];// 2
+             curr[buy][cap]= max(sell,keep);                
             }
-
         }
-    return dp[0][1][0];
+    }
+    next=curr;
+        }
+    return next[1][0];
     }
 };
