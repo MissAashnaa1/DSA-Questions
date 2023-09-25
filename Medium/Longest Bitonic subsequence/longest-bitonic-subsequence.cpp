8155d62b7a45948434d56317932b520e3898f239
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-26 04:28:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	int LongestBitonicSequence(vector<int>arr)
+	{
+	    // code here
+	    int n=arr.size();
+	vector<int> dp1(n,1);
+	vector<int> dp2(n,1);
+    int ans = 1;
+    for(int i=0; i<=n-1; i++){
+        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
+            
+            if(arr[prev_index]<arr[i]){
+                dp1[i] = max(dp1[i], 1 + dp1[prev_index]);
+                
+            }
+        }
+    
+    }
+
+    for(int i=n-1; i>=0; i--){
+        for(int prev_index = n-1; prev_index >i; prev_index--){
+            
+            if(arr[prev_index]<arr[i]){
+                dp2[i] = max(dp2[i], 1 + dp2[prev_index]);
+                
+            }
+        }
+     
+    
+    }
+    for(int i=0;i<n;i++){
+        ans=max(ans,dp2[i]+dp1[i]-1);
+    }
+    
+    return ans;
+    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution ob;
+		int ans = ob.LongestBitonicSequence(nums);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
