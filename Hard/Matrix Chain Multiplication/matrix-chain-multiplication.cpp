09aa50b4f47a59fd02223d73aaebe6001f7da861
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-26 12:04:49 +0530

    Updated Solution - GfG to GitHub

--- Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -8,26 +8,26 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-    int solve(int arr[],int i,int j,vector<vector<int>>&dp){
-        if(i>=j){
-            return 0;
-        }
-        int mixans=INT_MAX;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        for(int k=i;k<j;k++){
-            int temp= arr[i-1]*arr[k]*arr[j]+solve(arr,i,k,dp)+solve(arr,k+1,j,dp);
-            mixans=min(mixans,temp);
-        }
-        return dp[i][j]=mixans;
-    }
 public:
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        vector<vector<int>>dp(N,vector<int>(N,-1));
-        return solve(arr,1,N-1,dp);
+        vector<vector<int>>dp(N,vector<int>(N,0));
+        for(int i=0;i<N;i++){
+            dp[i][i]=0;
+        }
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<=N-1;j++){
+                int minans=INT_MAX;
+                for(int k=i;k<j;k++){
+                    int temp= arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    minans=min(minans,temp);
+                }
+                dp[i][j]=minans;
+            }
+            
+        }
+        return dp[1][N-1];
     }
 };
 
