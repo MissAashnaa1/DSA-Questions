4aa5ad7a226c016b7f7e18d26c59a5bb69841e2c
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-09-29 00:07:15 +0530

    Time: 259 ms (74.43%), Space: 10.8 MB (5.45%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -19,7 +19,21 @@ public:
       
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
-        return helper(1,nums.size()-2,nums,dp);
+        // vector<vector<int>>dp(nums.size(),vector<int>(nums.size(),-1));
+        // return helper(1,nums.size()-2,nums,dp);
+        vector<vector<int>>dp(nums.size()+2,vector<int>(nums.size()+2,0));
+        for(int left=nums.size()-2;left>=1;left--){
+            for(int right=1;right<=nums.size()-2;right++){
+                if(left>right)
+                    continue;
+                    int maxCost=INT_MIN;
+                    for(int i=left;i<=right;i++){
+                        maxCost=max(maxCost,nums[left-1]*nums[i]*nums[right+1]+dp[left][i-1]+dp[i+1][right]);
+                    }
+                     dp[left][right]=maxCost;
+                }
+            }
+        
+        return dp[1][nums.size()-2];
     }
 };
