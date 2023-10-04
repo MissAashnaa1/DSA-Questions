dc19fb9e9e9593d38a912cd306e13cd79758e36c
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2023-10-04 07:56:09 +0530

    Time: 9 ms (79.04%), Space: 9 MB (46.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,21 +6,29 @@ public:
     int j=0;
     int maxl=0;
     map<char,int>m;
-    while(j<s.length()){
-        m[s[j]]++;
-        if(j-i+1==m.size()){
-        maxl=max(maxl,j-i+1);
+    // while(j<s.length()){
+    //     m[s[j]]++;
+    //     if(j-i+1==m.size()){
+    //     maxl=max(maxl,j-i+1);
         
+    //     }
+    //     else {
+    //         while(j-i+1!=m.size()){
+    //              m[s[i]]--;
+    //         if(m[s[i]]==0){
+    //           m.erase(s[i]);
+    //             }
+    //         i++;
+    //         }
+    //     }
+    //     j++;
+    // }
+    while(j<s.length()){
+        if(m.find(s[j])!=m.end()){
+            i = max(i, m[s[j]] + 1);
         }
-        else {
-            while(j-i+1!=m.size()){
-                 m[s[i]]--;
-            if(m[s[i]]==0){
-              m.erase(s[i]);
-                }
-            i++;
-            }
-        }
+        m[s[j]]=j;
+        maxl=max(maxl,j-i+1);
         j++;
     }
     return maxl;
