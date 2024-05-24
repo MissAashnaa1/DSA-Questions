36c3074716fbf2ef60c00f3f9fbc81c01884761a
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2024-05-24 20:33:14 +0530

    Updated Solution - GfG to GitHub

--- Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -5,32 +5,28 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+      int mod= 1e9+7;
+    int help(int i, int j, int n, int t, vector<int>&arr, vector<vector<int>>&dp){
+        if(i==n) return j==t;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int notPick= help(i+1,j, n,t, arr, dp)%mod;
+        int pick= help(i+1, j+arr[i], n,t,arr, dp) % mod;
+        
+        return dp[i][j]= (pick + notPick)%mod;
+        
+    }
     int countPartitions(int n, int d, vector<int>& arr) {
         // Code here
-         const int mod = 1e9+7;
-        int sum = 0;
-        for(int i = 0 ; i< n; i++) sum += arr[i];
-        sum += d;
+        int sum= accumulate(arr.begin(), arr.end(), 0);
         
-        if(sum%2==1)
-            return 0;
-            sum/=2;
-        vector<int>dp(sum+1,0);
-        dp[0]+=1;
-        dp[arr[0]]+=1;
-        for(int i=1;i<arr.size();i++){
-            vector<int>ndp(sum+1,0);
-            for(int target=0;target<=sum;target++){
-                
-                int notTake=dp[target];
-                int take=0;
-                if(target>=arr[i])
-                    take=dp[target-arr[i]];
-                ndp[target]=(take+notTake)%mod;
-            }
-            dp=ndp;
-        }
-        return dp[sum];
+        vector<vector<int>>dp(n, vector<int>(sum+1, -1));
+        sum=sum-d;
+        if(sum<0 or sum&1) return 0;
+        
+        sum=sum/2;
+        return help(0, 0,n,sum,arr,dp);
     }
 };
 
