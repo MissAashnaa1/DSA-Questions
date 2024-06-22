97b2901b52b77fa7549f9d9300a8250e677d59bc
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2024-06-22 19:07:53 +0530

    Time: 176 ms (11.07%), Space: 93.9 MB (12.05%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int odd=0;
-        int ans=0;
-        unordered_map<int,int>m;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]%2){
-                odd++;
+        int numberOfSubarrays(vector<int>& nums, int k) {
+            int odd=0;
+            int ans=0;
+            unordered_map<int,int>m;
+            for(int i=0;i<nums.size();i++){
+                if(nums[i]%2){
+                    odd++;
+                }
+                m[odd]++;
+                if(odd==k){
+                    ans++;
+                }
+                if(m.find(odd-k)!=m.end()){
+                    ans+=m[odd-k];
+                }
             }
-            m[odd]++;
-            if(odd==k){
-                ans++;
-            }
-            if(m.find(odd-k)!=m.end()){
-                ans+=m[odd-k];
-            }
+        return ans;
         }
-    return ans;
-    }
 };
