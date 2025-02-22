59dc4a512b2aadedbb7b2e9bf41663d1d31c7e58
Aashnaa <97970004+MissAashnaa1@users.noreply.github.com>
2025-02-22 19:30:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -6,31 +6,57 @@ using namespace std;
 
 
 // } Driver Code Ends
-// User function Template for C++
 
 class Solution {
   public:
-    int maxLength(string& str) {
-        int ans=0,op=0,cl=0;
-        for(int i=0;i<str.length();i++)
-        {
-            if(str[i]=='(') op++;
-            else cl++;
-            if(cl>op) op=cl=0;
-            if(cl==op) ans=max(ans,cl*2);
+       int maxLength(string& s) {
+        
+        int n = s.length();
+        vector<int> dp(n, 0);
+        
+        stack<pair<char,int>> st;
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            
+            if(s[i] == '(') {
+                st.push({'(', cnt});
+                cnt++;
+            }
+            else {
+                
+                if(!st.empty()) {
+                    pair<char,int> pt = st.top();
+                    if(!st.empty() && pt.first == '(') {
+                        dp[pt.second] = 2;
+                        st.pop();
+                    }
+                    else {
+                        cnt++;
+                    }
+                }
+                else {
+                    cnt++;
+                }
+                
+            }
         }
-        op=cl=0;
-        for(int i=str.length()-1;i>=0;i--)
+        
+        int ans = INT_MIN;
+        int sum = 0;
+        for(int i = 0; i < n; i++)
         {
-            if(str[i]=='(')op++;
-            else cl++;
-            if(cl<op) op=cl=0;
-            if(cl==op) ans=max(ans,cl*2);
+            sum += dp[i];
+            ans = max(ans, sum);
+            
+            if(dp[i] == 0) {
+                sum = 0;
+            }
+ 
         }
+        
         return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 
@@ -43,6 +69,9 @@ int main() {
 
         Solution ob;
         cout << ob.maxLength(S) << "\n";
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
